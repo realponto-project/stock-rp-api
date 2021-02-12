58e5b5ae2ca1d00eae35e5160580ee854ccc0023
Alexandre Soares <ale_santos.soares@hotmail.com>
2021-02-12 07:02:53 -0300

    devops ci

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,40 @@
 require('dotenv').config()
 
 module.exports = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 5432,
-  password: process.env.DB_PASSWORD,
-  username: process.env.DB_USERNAME,
-  database: process.env.DB_DATABASE,
-  dialect: 'postgres',
-  // operatorsAliases: false,
-  logging: false,
-  define: {
-    timestamps: true
-    // underscored: true,
-    // underscoredAll: true
+  development: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    password: process.env.DB_PASSWORD,
+    username: process.env.DB_USERNAME,
+    database: process.env.DB_DATABASE,
+    dialect: 'postgres',
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 1000000,
+      idle: 10000
+    },
+    define: {
+      freezeTableName: true,
+      paranoid: true,
+      timestamps: true
+    }
+  },
+  test: {
+    "use_env_variable": "DATABASE_URL",
+    dialectOptions: {
+      ssl: true,
+    },
+  },
+  production: {
+    "use_env_variable": "DATABASE_URL",
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+          require:true,
+          rejectUnauthorized: false
+      },
+    },
   }
 }
